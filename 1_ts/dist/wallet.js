859cf37f7160b83a491d5c651405889336fdea7e
Marco Bagni <marco@lostconversation.com>
2025-07-31 15:44:44 +0200

    NFT Marketplace

--- 1_ts/dist/wallet.js
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs";
-import { homedir } from "os";
-//import * as os from "os";
-//import * as path from "path";
-const walletPath = `${homedir()}/.config/solana/d1x.json`; // alternative
-//const walletPath = path.join(os.homedir(), ".config", "solana", "d1x.json");
-const wallet = JSON.parse(fs.readFileSync(walletPath, "utf-8"));
-export default wallet;
